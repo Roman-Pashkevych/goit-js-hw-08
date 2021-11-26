71a4f528c337ae9e5b7fe28c2d6ca47398ad0b0d
Roman-Pashkevych <pashkevych_roman@ukr.net>
2021-11-26 13:40:40 +0200

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,6 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
-// const vimeoPlayer = document.querySelector('#vimeo-player');
-// const player = new Player(vimeoPlayer);
-// const LOCAL_STORAGE_KEY = "videoplayer-current-time"
-// const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
-
-
-// player.setCurrentTime(currentTime);
-
-// player.on('timeupdate', throttle(data => localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds), 1000));
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,51 +1,5 @@
 import throttle from 'lodash.throttle';
 
-/* const STORAGE_KEY = "feedback-form-state";
-const form = document.querySelector('.feedback-form');
-const input = document.querySelector('input');
-const textarea = document.querySelector('textarea');
-const feedbackFormData = {};
-
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onFormInput, 500));
-
-populateForm();
-
-function onFormSubmit(event) {
-    event.preventDefault();
-    
-    const formEl = event.currentTarget.elements;
-    const email = formEl.email.value;
-    const message = formEl.message.value;
-    const formData = {
-        email,
-        message,
-    };
-    
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-}
-
-function onFormInput(event) {
-    feedbackFormData[event.target.name] = event.target.value;
-    console.log(feedbackFormData);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
-}
-
-function populateForm() {
-    const saveFormData = localStorage.getItem(STORAGE_KEY);
-    const saveFormDataPars = JSON.parse(saveFormData);
-     
-    
-    if (saveFormDataPars) {
-    input.value = saveFormDataPars.email;
-    textarea.value = saveFormDataPars.message;
-    feedbackFormData.message = saveFormDataPars.message;
-    feedbackFormData.email = saveFormDataPars.email;
-        
-};
-} */
-
 const storage_Key = 'feedback-form-state';
 
 const formRef = document.querySelector('form');
